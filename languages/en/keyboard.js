0a9a37f3ddbad2d21b09e230268558939ce1cc2a
trentreimer <48562882+trentreimer@users.noreply.github.com>
2026-04-07 16:59:08 -0500

    Language support

--- languages/en/keyboard.js
+++ languages/en/keyboard.js
@@ -1,12 +1,10 @@
 export const keyboard = [
-    [
-        ['A', 'B', 'C', 'D', 'E', 'F'],
-        ['G', 'H', 'I', 'J', 'K', 'L'],
-        ['M', 'N', 'O', 'P', 'Q', 'R'],
-        ['S', 'T', 'U', 'V', 'W', 'X'],
-        ['Y', 'Z', '?', '.', ',', '\'', '!'],
-        ['_', '⌫', '↵', '<i class="fas fa-pause" data-character="Pause"></i>', '<i class="fas fa-cut" data-character="Cut"></i>', '🛑'],
-    ],
+    ['A', 'B', 'C', 'D', 'E', 'F'],
+    ['G', 'H', 'I', 'J', 'K', 'L'],
+    ['M', 'N', 'O', 'P', 'Q', 'R'],
+    ['S', 'T', 'U', 'V', 'W', 'X'],
+    ['Y', 'Z', '?', '.', ',', '\'', '!'],
+    ['_', '⌫', '↵', '<i class="fas fa-pause" data-character="Pause"></i>', '<i class="fas fa-trash" data-character="Empty"></i>', '🛑'],
     /*[
         ['A', 'B', 'C', 'D', 'E', 'F'],
         ['G', 'H', 'I', 'J', 'K', 'L'],
@@ -17,6 +15,6 @@ export const keyboard = [
         ['Y', 'Z', '.', '?', '!', ',', ':', ';'],
         ['_', '+', '-', '=', '"', '\'', '@', '#', '&'],
         ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
-        ['⌫', '↵', '<i class="fas fa-pause" data-character="Pause"></i>', '<i class="fas fa-trash" data-character="Cut"></i>', '🛑'],
+        ['⌫', '↵', '<i class="fas fa-pause" data-character="Pause"></i>', '<i class="fas fa-trash" data-character="Empty"></i>', '🛑'],
     ],*/
 ];
